0893ed4e326e282f0e2146240e9a96be76be2f8c
Vasudev08 <vasu14devagarwal@gmail.com>
2025-11-22 15:29:28 -0600

    updating the colab pipline

--- agentic_image2dataset/models/super_resolution/adcsr.py
+++ agentic_image2dataset/models/super_resolution/adcsr.py
@@ -60,8 +60,8 @@ class AdcSRModel(BaseProcessingModel):
         device = torch.device(self.device)
 
         # Load Stable Diffusion pipeline
-        # model_id = "stabilityai/stable-diffusion-2-1-base"
-        model_id = "Manojb/stable-diffusion-2-1-base"
+        model_id = "stabilityai/stable-diffusion-2-1-base"
+        # model_id = "Manojb/stable-diffusion-2-1-base"
         pipe = StableDiffusionPipeline.from_pretrained(model_id).to(device)
 
         unet = pipe.unet
